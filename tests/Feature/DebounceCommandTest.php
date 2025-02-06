3e261b26448fe46f7e4c58c0d22e1f88cc58d87e
zackaj <aktouf98zakaria@gmail.com>
2025-02-06 17:01:29 +0100

    add testing for debouncing from cli

--- tests/Feature/DebounceCommandTest.php
+++ tests/Feature/DebounceCommandTest.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Command;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Queue;
+use Zackaj\LaravelDebounce\Commands\DebounceConsoleCommand;
 use Zackaj\LaravelDebounce\DebounceCommand;
 use Zackaj\LaravelDebounce\Facades\Debounce;
 use Zackaj\LaravelDebounce\Tests\BaseCase;
@@ -56,6 +57,44 @@ class DebounceCommandTest extends BaseCase
         Debounce::command('dtest:test', 0, 'key', ['word' => 'test arg'], false);
 
         $this->assertTrue(DCommand::$fired);
+    }
+
+    public function test_debounce_from_cli_is_debounced_and_fired()
+    {
+        Queue::fake();
+        Artisan::registerCommand(new NormalCommand);
+        Artisan::registerCommand(new DCommand);
+        Artisan::registerCommand(new DebounceConsoleCommand);
+        $commands = [
+            ['signature' => 'test:test', 'class' => NormalCommand::class],
+            ['signature' => 'dtest:test', 'class' => DCommand::class],
+        ];
+
+        foreach ($commands as $key => $cmd) {
+            //for future tests
+            $args = [
+                'command' => $cmd['signature'],
+                'delay' => 0,
+                'uniqueKey' => 'key',
+                'parameters' => [
+                    'word' => 'hello',
+                ],
+            ];
+
+            $commandString = sprintf(
+                'debounce:command %s %s %s %s',
+                $args['delay'],
+                $args['uniqueKey'],
+                $args['command'],
+                $args['parameters']['word'],
+            );
+
+            $this->artisan($commandString)->assertSuccessful();
+            $this->artisan($commandString)->assertSuccessful();
+            Queue::assertCount($key + 1);
+
+            $this->assertTrue($cmd['class']::$fired);
+        }
     }
 }
 
